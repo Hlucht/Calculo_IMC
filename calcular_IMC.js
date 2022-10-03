f657d3e5babe6ba64e599e6d0bbc8305d2190863
Hlucht <luchtembergh@gmail.com>
2022-10-03 14:54:43 -0300

    Adicionando arquivo do exercicio

---
+++ calcular_IMC.js
@@ -0,0 +1,44 @@
+
+
+function preenche(){
+const objeto = {
+    nome: prompt(" Informe seu nome: "),
+    idade: prompt(" Informe sua idade"),
+    peso: prompt(" Informe seu peso: "),
+    altura: prompt(" Informe sua altura: ")
+}
+return objeto;
+}
+
+function calculo(objeto){
+const resultado = objeto.peso/(objeto.altura * objeto.altura);
+
+if(resultado < 18.5){
+    alert(objeto.nome+ " seu indice de maca corporal esta abaixo do normal"+ " -- "+ resultado.toFixed(2));
+
+}else if(resultado >= 18.5 && resultado <= 24.9){
+    alert(objeto.nome+ " seu indice de maca corporal esta normal"+ " -- "+ resultado.toFixed(2));
+
+}else if(resultado >24.9 && resultado <= 29.9){
+    alert(objeto.nome+ " seu indice de maca corporal esta em sobrepeso"+ " -- "+ resultado.toFixed(2));
+
+}else if(resultado >29.9 && resultado <= 39.9){
+    alert(objeto.nome+ " seu indice de maca corporal esta em obesidade"+ " -- "+ resultado.toFixed(2));
+
+}else if(resultado >39.9){
+    alert(objeto.nome+ " seu indice de maca corporal esta em obesidade grave"+ " -- "+ resultado.toFixed(2));
+}
+return resultado;
+}
+
+const lista=[];
+for(let i=0; i<10 ;i++){
+    lista[i] = preenche() 
+    lista[i].resultado = calculo(lista[i])
+    alert(" A lista possui "+ (10 - i)+ " posicoes livres");
+}
+
+alert(" Abra seu console na para vizualizar a lista !");
+
+console.log(lista);
+
